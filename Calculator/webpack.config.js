fb2e486f6d313231579ef854f2621aec06da9e7e
Andy Lin <Andy.Lin@xuenn.com>
2017-06-16 16:52:44 +0800

    [Homework] Environment Enhancement

--- Calculator/webpack.config.js
+++ Calculator/webpack.config.js
@@ -29,17 +29,7 @@ module.exports =
 
     plugins:
     [
-        new webpack.HotModuleReplacementPlugin(),
-
-        new webpack.DefinePlugin
-        (
-            {
-                "process.env":
-                {
-                    "NODE_ENV": JSON.stringify("development")
-                }
-            }
-        )
+        new webpack.HotModuleReplacementPlugin()
     ],
 
     module:
