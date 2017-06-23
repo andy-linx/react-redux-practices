5589d7e0da3936f440ecf038f6f3c629f9f64bda
Andy Lin <Andy.Lin@xuenn.com>
2017-06-23 12:17:25 +0800

    [Homework] Environment Enhancement

--- Calculator/.config/webpack.dev.js
+++ Calculator/.config/webpack.dev.js
@@ -9,31 +9,13 @@ module.exports =
     {
         "app":
         [
-            // Pay attention on "react-hot-loader" must be the first one always.
             "react-hot-loader/patch",
-
-            // This path would indicate to the virtaul path, "~/js/app-index.js", not default view page.
             path.resolve(__dirname, "../js/app-index"),
-
             `webpack-dev-server/client?http://${settings.host}:${settings.port}`,
             "webpack/hot/only-dev-server"
         ]
     },
 
-    // There're 7 modes to configure.
-    //
-    // (1) eval
-    // (2) cheap-eval-source-map
-    // (3) cheap-source-map
-    // (4) cheap-module-eval-source-map
-    // (5) cheap-module-source-map
-    // (6) eval-source-map
-    // (7) source-map
-    //
-    // [Implementation 1st]
-    // devtool: process.env["npm_package_config_source-map"] || false,
-    //
-    // [Implementation 2nd]
     devtool: process.env.VARS ? JSON.parse(process.env.VARS)["SMAP"] || false : false,
 
     output:
